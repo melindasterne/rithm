d636c5821bdfc3fb13fd32893bce097e5a7e83a9
melindasterne <melindasterne@melindasternes-MacBook-Pro.local>
2019-08-06 10:16:03 -0700

    add index.html and first.js files

---
+++ first.js
@@ -0,0 +1,6 @@
+alert('hello world from first JS!');
+var firstName = 'Matt';
+
+console.log('Hi, ' + firstName + '!');
+console.log('How are you doing ' + firstName + '?');
+console.log('See you later, ' + firstName + '!');
