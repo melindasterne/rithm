8cd9a76a27b884887767461ce27642175cf09423
melindasterne <melindasterne@melindasternes-MacBook-Pro.local>
2019-08-06 12:17:16 -0700

    add variables and primitives notes

--- first.js
+++ first.js
@@ -1,6 +1,30 @@
-alert('hello world from first JS!');
-var firstName = 'Matt';
+/* alert('hello world from first JS!');
+var firstName = prompt("What is your first name?");
+//this is a single line JS comment
+var lastName = 'Lane';
+var fullName = firstName + '' + lastName;
 
 console.log('Hi, ' + firstName + '!');
 console.log('How are you doing ' + firstName + '?');
-console.log('See you later, ' + firstName + '!');
+console.log('See you later, ' + firstName + '!'); */
+
+/*This is the start of
+a multiline comment, and
+this is the ending. */
+
+/* some primitive data types listed below
+string - number - boolean - undefined - null 
+the \ is an escape character
+console.log('Matt said "this is melinda\'s computer" the other day'); */
+
+var name = 'baloo';
+var dayOfBirth = '7/4/2011';
+var favoriteColor = prompt('what is your favorite color?');
+console.log(favoriteColor + " is a really pretty color!");
+var speak = name + ' said: "Melinda\'s favorite color is ' + favoriteColor + ', it\'s a really pretty color!"';
+console.log(speak);
+
+// null has no value
+// undefined has not been assigned a value
+// NaN stands for not a number
+// typeof will tell you what primitive the data type is
